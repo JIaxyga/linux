7ab0ea5a8ff4efadf201a52e4187fd0f609a3b82
Danila Tikhonov <danila@jiaxyga.com>
2023-07-28 23:27:15 +0300

    clk: videocc-sm7150: fix a bit

--- drivers/clk/qcom/videocc-sm7150.c
+++ drivers/clk/qcom/videocc-sm7150.c
@@ -1,17 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2023, Danila Tikhonov <danila@jiaxyga.com>
  */
-
-#define pr_fmt(fmt) "clk: %s: " fmt, __func__
 
 #include <linux/kernel.h>
 #include <linux/bitops.h>
@@ -24,7 +15,7 @@
 #include <linux/clk-provider.h>
 #include <linux/regmap.h>
 
-#include <dt-bindings/clock/qcom,videocc-sdmmagpie.h>
+#include <dt-bindings/clock/qcom,sm7150-videocc.h>
 
 #include "common.h"
 #include "clk-regmap.h"
@@ -32,13 +23,6 @@
 #include "clk-rcg.h"
 #include "clk-branch.h"
 #include "clk-alpha-pll.h"
-#include "vdd-level-sdmmagpie.h"
-
-#define F(f, s, h, m, n) { (f), (s), (2 * (h) - 1), (m), (n) }
-
-#define IRIS_DISBALE_MULTIPIPE	10
-
-static DEFINE_VDD_REGULATORS(vdd_cx, VDD_NUM, 1, vdd_corner);
 
 enum {
 	P_BI_TCXO,
@@ -81,7 +65,7 @@ static struct pll_vco fabia_vco[] = {
 
 static struct alpha_pll_config video_pll0_config = {
 	.l = 0x19,
-	.frac = 0x0,
+	.alpha = 0x0,
 	.config_ctl_val = 0x20485699,
 	.config_ctl_hi_val = 0x00002067,
 	.user_ctl_val = 0x00000001,
@@ -93,20 +77,12 @@ static struct clk_alpha_pll video_pll0 = {
 	.offset = 0x42c,
 	.vco_table = fabia_vco,
 	.num_vco = ARRAY_SIZE(fabia_vco),
-	.type = FABIA_PLL,
 	.clkr = {
 		.hw.init = &(struct clk_init_data){
 			.name = "video_pll0",
 			.parent_names = (const char *[]){ "bi_tcxo" },
 			.num_parents = 1,
-			.ops = &clk_fabia_pll_ops,
-			.vdd_class = &vdd_cx,
-			.num_rate_max = VDD_NUM,
-			.rate_max = (unsigned long[VDD_NUM]) {
-				[VDD_MIN] = 615000000,
-				[VDD_LOW] = 1066000000,
-				[VDD_LOW_L1] = 1600000000,
-				[VDD_NOMINAL] = 2000000000},
+			.ops = &clk_alpha_pll_fabia_ops,
 		},
 	},
 };
@@ -130,21 +106,12 @@ static struct clk_rcg2 video_cc_iris_clk_src = {
 	.hid_width = 5,
 	.parent_map = video_cc_parent_map_0,
 	.freq_tbl = ftbl_video_cc_iris_clk_src,
-	.enable_safe_config = true,
 	.clkr.hw.init = &(struct clk_init_data){
 		.name = "video_cc_iris_clk_src",
 		.parent_names = video_cc_parent_names_0,
 		.num_parents = 5,
 		.flags = CLK_SET_RATE_PARENT,
 		.ops = &clk_rcg2_ops,
-		.vdd_class = &vdd_cx,
-		.num_rate_max = VDD_NUM,
-		.rate_max = (unsigned long[VDD_NUM]) {
-			[VDD_LOWER] = 240000000,
-			[VDD_LOW] = 338000000,
-			[VDD_LOW_L1] = 365000000,
-			[VDD_NOMINAL] = 444000000,
-			[VDD_HIGH] = 533000000},
 	},
 };
 
@@ -291,25 +258,7 @@ static struct clk_branch video_cc_venus_ahb_clk = {
 	},
 };
 
-static struct clk_branch video_cc_xo_clk = {
-	.halt_reg = 0x984,
-	.halt_check = BRANCH_HALT,
-	.clkr = {
-		.enable_reg = 0x984,
-		.enable_mask = BIT(0),
-		.hw.init = &(struct clk_init_data){
-			.name = "video_cc_xo_clk",
-			.parent_names = (const char *[]){
-				"video_cc_xo_clk_src",
-			},
-			.num_parents = 1,
-			.flags = CLK_SET_RATE_PARENT | CLK_IS_CRITICAL,
-			.ops = &clk_branch2_ops,
-		},
-	},
-};
-
-static struct clk_regmap *video_cc_sdmmagpie_clocks[] = {
+static struct clk_regmap *video_cc_sm7150_clocks[] = {
 	[VIDEO_PLL0] = &video_pll0.clkr,
 	[VIDEO_CC_IRIS_AHB_CLK] = &video_cc_iris_ahb_clk.clkr,
 	[VIDEO_CC_IRIS_CLK_SRC] = &video_cc_iris_clk_src.clkr,
@@ -320,11 +269,10 @@ static struct clk_regmap *video_cc_sdmmagpie_clocks[] = {
 	[VIDEO_CC_MVSC_CORE_CLK] = &video_cc_mvsc_core_clk.clkr,
 	[VIDEO_CC_MVSC_CTL_AXI_CLK] = &video_cc_mvsc_ctl_axi_clk.clkr,
 	[VIDEO_CC_VENUS_AHB_CLK] = &video_cc_venus_ahb_clk.clkr,
-	[VIDEO_CC_XO_CLK] = &video_cc_xo_clk.clkr,
 	[VIDEO_CC_XO_CLK_SRC] = &video_cc_xo_clk_src.clkr,
 };
 
-static const struct regmap_config video_cc_sdmmagpie_regmap_config = {
+static const struct regmap_config video_cc_sm7150_regmap_config = {
 	.reg_bits	= 32,
 	.reg_stride	= 4,
 	.val_bits	= 32,
@@ -332,108 +280,58 @@ static const struct regmap_config video_cc_sdmmagpie_regmap_config = {
 	.fast_io	= true,
 };
 
-static const struct qcom_cc_desc video_cc_sdmmagpie_desc = {
-	.config = &video_cc_sdmmagpie_regmap_config,
-	.clks = video_cc_sdmmagpie_clocks,
-	.num_clks = ARRAY_SIZE(video_cc_sdmmagpie_clocks),
+static const struct qcom_cc_desc video_cc_sm7150_desc = {
+	.config = &video_cc_sm7150_regmap_config,
+	.clks = video_cc_sm7150_clocks,
+	.num_clks = ARRAY_SIZE(video_cc_sm7150_clocks),
 };
 
-static const struct of_device_id video_cc_sdmmagpie_match_table[] = {
-	{ .compatible = "qcom,videocc-sdmmagpie" },
+static const struct of_device_id video_cc_sm7150_match_table[] = {
+	{ .compatible = "qcom,videocc-sm7150" },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, video_cc_sdmmagpie_match_table);
+MODULE_DEVICE_TABLE(of, video_cc_sm7150_match_table);
 
-static int video_multipipe_fixup(struct platform_device *pdev,
-				struct regmap *regmap)
-{
-	void __iomem *base;
-	struct resource *res;
-	struct device *dev = &pdev->dev;
-	u32 val;
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
-	base = devm_ioremap_resource(dev, res);
-	if (IS_ERR(base))
-		return PTR_ERR(base);
-
-	val = readl_relaxed(base);
-	val = (val >> IRIS_DISBALE_MULTIPIPE) & 0x1;
-
-	if (val) {
-		video_pll0_config.l = 0x14;
-		video_cc_iris_clk_src.freq_tbl =
-				ftbl_video_cc_iris_multipipe_clk_src;
-		video_cc_iris_clk_src.clkr.hw.init->rate_max[VDD_LOWER] =
-				200000000;
-		video_cc_iris_clk_src.clkr.hw.init->rate_max[VDD_LOW] =
-				200000000;
-		video_cc_iris_clk_src.clkr.hw.init->rate_max[VDD_LOW_L1] =
-				200000000;
-		video_cc_iris_clk_src.clkr.hw.init->rate_max[VDD_NOMINAL] =
-				200000000;
-		video_cc_iris_clk_src.clkr.hw.init->rate_max[VDD_HIGH] =
-				200000000;
-	}
-
-	clk_fabia_pll_configure(&video_pll0, regmap, &video_pll0_config);
-
-	return 0;
-}
-
-static int video_cc_sdmmagpie_probe(struct platform_device *pdev)
+static int video_cc_sm7150_probe(struct platform_device *pdev)
 {
 	struct regmap *regmap;
 	int ret;
 
-	vdd_cx.regulator[0] = devm_regulator_get(&pdev->dev, "vdd_cx");
-	if (IS_ERR(vdd_cx.regulator[0])) {
-		if (PTR_ERR(vdd_cx.regulator[0]) != -EPROBE_DEFER)
-			dev_err(&pdev->dev,
-					"Unable to get vdd_cx regulator\n");
-		return PTR_ERR(vdd_cx.regulator[0]);
-	}
-
-	regmap = qcom_cc_map(pdev, &video_cc_sdmmagpie_desc);
+	regmap = qcom_cc_map(pdev, &video_cc_sm7150_desc);
 	if (IS_ERR(regmap)) {
 		pr_err("Failed to map the video_cc registers\n");
 		return PTR_ERR(regmap);
 	}
 
-	ret = video_multipipe_fixup(pdev, regmap);
-	if (ret)
-		return ret;
+	video_pll0_config.l = 0x1f;
 
-	ret = qcom_cc_really_probe(pdev, &video_cc_sdmmagpie_desc, regmap);
-	if (ret) {
-		dev_err(&pdev->dev, "Failed to register Video CC clocks\n");
-		return ret;
-	}
+	/* Keep video_cc_xo_clk always enabled */
+	regmap_update_bits(regmap, 0x984, BIT(0), BIT(0));
 
-	dev_info(&pdev->dev, "Registered Video CC clocks\n");
+	ret = qcom_cc_really_probe(pdev, &video_cc_sm7150_desc, regmap);
+
 	return ret;
 }
 
-static struct platform_driver video_cc_sdmmagpie_driver = {
-	.probe = video_cc_sdmmagpie_probe,
+static struct platform_driver video_cc_sm7150_driver = {
+	.probe = video_cc_sm7150_probe,
 	.driver = {
-		.name = "video_cc-sdmmagpie",
-		.of_match_table = video_cc_sdmmagpie_match_table,
+		.name = "video_cc-sm7150",
+		.of_match_table = video_cc_sm7150_match_table,
 	},
 };
 
-static int __init video_cc_sdmmagpie_init(void)
+static int __init video_cc_sm7150_init(void)
 {
-	return platform_driver_register(&video_cc_sdmmagpie_driver);
+	return platform_driver_register(&video_cc_sm7150_driver);
 }
-subsys_initcall(video_cc_sdmmagpie_init);
+subsys_initcall(video_cc_sm7150_init);
 
-static void __exit video_cc_sdmmagpie_exit(void)
+static void __exit video_cc_sm7150_exit(void)
 {
-	platform_driver_unregister(&video_cc_sdmmagpie_driver);
+	platform_driver_unregister(&video_cc_sm7150_driver);
 }
-module_exit(video_cc_sdmmagpie_exit);
+module_exit(video_cc_sm7150_exit);
 
-MODULE_DESCRIPTION("QTI VIDEO_CC SDMMAGPIE Driver");
-MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:video_cc-sdmmagpie");
+MODULE_DESCRIPTION("QTI VIDEOCC SM7150 Driver");
+MODULE_LICENSE("GPL");
